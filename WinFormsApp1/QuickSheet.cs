e0fd67dcbd35c579a582515f0a5c99d9328b0e1a
Jahnavi Malhotra <j7malhot@uwaterloo.ca>
2024-06-06 16:15:45 -0400

    Add project files.

---
+++ WinFormsApp1/QuickSheet.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class QuickSheet : Form
+    {
+        public QuickSheet()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
